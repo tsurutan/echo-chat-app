6e843a0a589b55d4f6ac04743f9a5ad34f9268d0
tsurutan <tsurutan.android@gmail.com>
2022-11-10 18:28:49 +0900

    fix: 🐛 Change the scope of variables in the test

--- controllers/user_controller_test.go
+++ controllers/user_controller_test.go
@@ -22,32 +22,32 @@ func (service *UserServiceDouble) GetUser(id int) (*models.User, error) {
 
 func TestUserControllerGet(t *testing.T) {
 	e := echo.New()
-	userService := &UserServiceDouble{
+	userServiceDouble := &UserServiceDouble{
 		GetUserCalledId: 0,
 	}
 	controller := &UserController{
-		UserService: userService,
+		UserService: userServiceDouble,
 	}
-	req := httptest.NewRequest(http.MethodGet, "/users/1234", nil)
-	var context echo.Context
-	var rec *httptest.ResponseRecorder
 
-	setup := func() {
-		rec = httptest.NewRecorder()
-		context = e.NewContext(req, rec)
+	createContext := func(rec *httptest.ResponseRecorder) echo.Context {
+		req := httptest.NewRequest(http.MethodGet, "/users/1234", nil)
+		context := e.NewContext(req, rec)
 		context.SetPath("/users/:id")
-		userService.GetUserReturnData = &models.User{}
+		userServiceDouble.GetUserReturnData = &models.User{}
+		return context
 	}
 
 	t.Run("should return status ok", func(t *testing.T) {
-		setup()
+		rec := httptest.NewRecorder()
+		context := createContext(rec)
 		controller.getUser(context)
 		assert.Equal(t, http.StatusOK, rec.Code)
 	})
 
 	t.Run("should return user json", func(t *testing.T) {
-		setup()
-		userService.GetUserReturnData = &models.User{
+		rec := httptest.NewRecorder()
+		context := createContext(rec)
+		userServiceDouble.GetUserReturnData = &models.User{
 			ID:   1234,
 			Name: "Yamada",
 		}
@@ -56,10 +56,11 @@ func TestUserControllerGet(t *testing.T) {
 	})
 
 	t.Run("should call user service by param id", func(t *testing.T) {
-		setup()
+		rec := httptest.NewRecorder()
+		context := createContext(rec)
 		context.SetParamNames("id")
 		context.SetParamValues("1234")
 		controller.getUser(context)
-		assert.Equal(t, 1234, userService.GetUserCalledId)
+		assert.Equal(t, 1234, userServiceDouble.GetUserCalledId)
 	})
 }
